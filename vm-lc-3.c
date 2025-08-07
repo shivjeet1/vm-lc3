c5256d17d871b6ade70e104aaf78e473fe16f670
shivjeet1 <shivamlavhalepatil@gmail.com>
2025-08-07 22:56:04 +0530

    added some includes, registers, condition flags and opcodes

--- vm-lc-3.c
+++ vm-lc-3.c
@@ -1,5 +1,7 @@
 #define MEMORY_MAX (1 << 16)
+
 uint16_t memory[MEMORY_MAX]; // 65536 locations
+
 
 enum {
   R_R0 = 0,
@@ -15,9 +17,33 @@ enum {
   R_COUNT
 };
 
+enum {
+  FL_POS = 1 << 0,
+  FL_ZRO = 1 << 1,
+  FL_NEG = 1 << 2,
+};
 
 uint16_t reg[R_COUNT];
 
+
+enum {
+  OP_BR = 0,
+  OP_ADD,
+  OP_LD,
+  OP_ST,
+  OP_JSR,
+  OP_AND,
+  OP_LDR,
+  OP_STR,
+  OP_RTI,
+  OP_NOT,
+  OP_LDI,
+  OP_STI,
+  OP_JMP,
+  OP_RES,
+  OP_LEA,
+  OP_TRAP
+};
 
 
 
