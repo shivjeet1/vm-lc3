9a056bc5b891185426ce51685f506b2e533130ba
shivjeet1 <shivamlavhalepatil@gmail.com>
2025-08-08 23:52:16 +0530

    added update_flags

--- vm-lc-3.c
+++ vm-lc-3.c
@@ -116,5 +116,18 @@ uint16_t swap16(uint16_t x)
   return (x << 8) | (x >> 8);
 }
 
-
-
+void update_flags(uint16_t r)
+{
+  if (reg[r] == 0)
+  {
+    reg[R_COND] = FL_ZRO;
+  }
+  else if (reg[r] >> 15)
+  {
+    reg[R_COND] = FL_NEG;
+  }
+  else
+  {
+    reg[R_COND] = FL_POS;
+  }
+}
