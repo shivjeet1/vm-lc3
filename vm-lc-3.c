9c6225449039147d7382e8b8e3a3cdb4a5fec893
shivjeet1 <shivamlavhalepatil@gmail.com>
2025-08-09 01:19:45 +0530

    added cases for some flags

--- vm-lc-3.c
+++ vm-lc-3.c
@@ -152,4 +152,113 @@ int main(int argc, const char* argv[])
   signal(SIGINT, handle_interrupt);
   disable_input_buffering();
 
+  /*since exactly one condition flag should be set at any given time, set the Z flag*/
+  reg[R_COND] = FL_ZRO;
+
+  /*set the PC to starting postion*/
+  //0x3000 is the default
+  enum {
+    PC_START = 0x3000
+  };
+
+  reg[R_PC] = PC_START;
+
+  int running = 1;
+  while (running)
+  {
+    //FETCH
+    uint16_t instr = mem_read(reg[R_PC]++);
+    uint16_t op = instr >> 12;
+
+    switch (op)
+    {
+      case OP_ADD:
+        {
+          /*destination register (DR) */
+          uint16_t r0 = (instr >> 9) & 0x7;
+          //first operand
+          uint16_t r1 = (instr >> 6) & 0x7;
+          //whether we are in immediate mode
+          uint16_t imm_flag = (instr >> 5) & 0x1;
+
+          if (imm_flag)
+          {
+            uint16_t imm5 = sign_extend(instr & 0x1F, 5);
+            reg[r0] = reg[r1] + imm5;
+          }
+          else 
+          {
+            uint16_t r2 = instr & 0x7;
+            reg[r0] = reg[r1] + reg[r2];
+          }
+          update_flags(r0);
+        }
+        break;
+
+      case OP_AND:
+        {
+          uint16_t r0 = (instr >> 9) & 0x7;
+          uint16_t r1 = (instr >> 6) & 0x7;
+          uint16_t imm_flag = (instr >> 5) & 0x1;
+
+          if (imm flag)
+          {
+            uint16_t imm5 = sign_extend(instr & 0x1F, 5);
+            reg[r0] = reg[r1] & imm5;
+          }
+          else {
+            uint16_t r2 = instr & 0x7;
+            reg[r0] = reg[r1] & reg[r2];
+          }
+          update_flags(r0);
+        }
+        break;
+
+      case OP_NOT:
+        {
+          uint16_t r0 = (instr >> 9) & 0x7;
+          uint16_t r1 = (instr >> 6) & 0x7;
+
+          reg[r0] = ~reg[r1];
+          update_flags(r0);
+        }
+        break;
+
+      case OP_BR:
+        {
+          uint16_t pc_offset = sign_extend(instr & 0x1FF, 9);
+          uint16_t cond_flag = (instr >> 9) & 0x7;
+          if (cond_flag & reg[R_COND])
+          {
+            reg[R_PC] += pc_offset;
+          }
+        }
+        break;
+
+      case OP_JMP:
+        {
+          uint16_t r1 = (instr >> 6) & 0x7;
+          reg[R_PC] = reg[r1];
+        }
+        break;
+
+      case OP_JSR:
+        {
+          uint16_t long_flag = (instr >> 11) & 1;
+          reg[R_R7] = reg[R_PC];
+          if (long_flag)
+          {
+            uint16_t long_pc_offset = sign_extend(instr & 0x7FF, 11);
+            reg[R_PC] += long_pc_offset;
+          }
+          else {
+            uint16_t r1 = (instr >> 6) & 0x7;
+            reg[R_PC] = reg[r1];
+          }
+        }
+        break;
+
+      
+    }
+  }
 }
