cf59802b3c916250c55ca913eff9fd6a5319c8d8
shivjeet1 <shivamlavhalepatil@gmail.com>
2025-08-08 23:44:35 +0530

    added check_key and disable_input_buffering

--- vm-lc-3.c
+++ vm-lc-3.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <signal.h>
-
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/termios.h>
+#include <sys/mman.h>
 
 enum {
   R_R0 = 0,
@@ -60,9 +66,55 @@ enum {
 };
 
 #define MEMORY_MAX (1 << 16)
-
 uint16_t memory[MEMORY_MAX]; // 65536 locations
+uint16_t reg[R_COUNT];
 
+struct termios original_tio;
+
+void disable_input_buffering()
+{
+  tcgetattr(STDIN_FILENO, &original_tio);
+  struct termios new_tio = original_tio;
+  new_tio.c_lflag &= ~ICANON & ~ECHO;
+  tcsetattr(STDIN_FILENO, TCSANOW, &new_tio);
+}
+
+void restore_input_buffering()
+{
+  tcsetattr(STDIN_FILENO, TCSANOW, &original_tio);
+}
+
+uint16_t check_key()
+{
+  fd_set readfds;
+  FD_ZERO(&readfds);
+  FD_SET(STDIN_FILENO, &readfds);
+
+  struct timeval timeout;
+  timeout.tv_sec = 0;
+  timeout.tv_usec = 0;
+  return select(1, &readfds, NULL, NULL, &timeout) != 0;
+}
+
+void handle_interrupt(int signal)
+{
+  restore_input_buffering();
+  printf("\n");
+  exit(-2);
+}
+
+uint16_t sign_extend(uint16_t x, int bit_count)
+{
+  if ((x >> (bit_count -1)) & 1) {
+    x |= (0xFFFF << bit_count);
+  }
+  return x;
+}
+
+uint16_t swap16(uint16_t x)
+{
+  return (x << 8) | (x >> 8);
+}
 
 
 
